8589d9a5a1a6d3ea101db878f1815593f7cb83a5
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-12-04 20:07:48 +0100

    Limpieza de imports en SettingFragment

--- WifiAware_code/app/src/main/java/d2d/testing/gui/setting/SettingFragment.java
+++ WifiAware_code/app/src/main/java/d2d/testing/gui/setting/SettingFragment.java
@@ -2,15 +2,9 @@ package d2d.testing.gui.setting;
 
 import android.os.Bundle;
 
-import androidx.preference.EditTextPreference;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceScreen;
-
-import javax.inject.Inject;
 
 import d2d.testing.R;
-import d2d.testing.gui.main.NetworkModule;
 
 public class SettingFragment extends PreferenceFragmentCompat{
 
